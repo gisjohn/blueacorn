e49f2a260f685e875a0c2445b04d2a6c877573fd
John Winningham <bill@genesis-internet.com>
2019-10-30 00:04:05 -0500

    Fix for CMS page install

--- app/code/RedAlmond/Cms/Setup/InstallData.php
+++ app/code/RedAlmond/Cms/Setup/InstallData.php
@@ -42,13 +42,13 @@ class InstallData implements \Magento\Framework\Setup\InstallDataInterface
             $cmsPageData = [
                 'title' => 'Style Guide',
                 'identifier' => 'style-guide',
-                'content' => "<style>.grid {  display: grid; grid-template-columns: 1fr 1fr 1fr; grid-gap: 40px; }</style>
+                'content' => '<style>.grid {  display: grid; grid-template-columns: 1fr 1fr 1fr; grid-gap: 40px; }</style>
 <h1>BUTTONS</h1>
 <div class="grid">
 <div class="col"><button class="button primary tocart" type="button"><span><span>Load More</span></span></button></div>
 <div class="col"><button class="button btn-continue" title="Continue Shopping" type="button"><span><span>Continue Shopping</span></span></button></div>
 <div class="col"><button class="action primary checkout" type="button">Checkout</button></div>
-</div>",
+</div>',
                 'is_active' => 1,
                 'stores' => [0],
                 'sort_order' => 0
